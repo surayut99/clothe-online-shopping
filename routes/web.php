7200f121b03a0d715f5edf3873ff6e12e2616d0c
arun0179 <kanyanat.i@ku.th>
2020-09-28 22:23:50 +0700

    update gitignore

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PagesController::class, 'index'])->name('pages.home');
 Route::get('/register', [PagesController::class, 'register'])->name('pages.register');
 Route::get('/login', [PagesController::class, 'login'])->name('pages.login');
-<<<<<<< HEAD
-=======
-Route::resource('pages', ProfileController::class);
->>>>>>> 989a49b77dd17b6490f9c4f34d1c7b03a5c5890d
