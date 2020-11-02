d115ece6938c83a846db8e20afc7d3907483ad9b
puen <surayut.bo@ku.th>
2020-11-02 15:46:12 +0700

    change data type of Cart table

--- database/migrations/2020_11_02_082551_create_carts_table.php
+++ database/migrations/2020_11_02_082551_create_carts_table.php
@@ -14,8 +14,8 @@ class CreateCartsTable extends Migration
     public function up()
     {
         Schema::create('carts', function (Blueprint $table) {
-            $table->string('customer_id')->nullable(false);
-            $table->string('product_id')->nullable(false);
+            $table->bigInteger('customer_id')->nullable(false);
+            $table->bigInteger('product_id')->nullable(false);
             $table->bigInteger('qty')->nullable(false);
 
             $table->primary(array('customer_id', 'product_id'));
