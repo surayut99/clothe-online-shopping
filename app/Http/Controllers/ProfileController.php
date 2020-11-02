cfa7c5e1dd3e77c375bf29bce69007c77ac8c437
puen <surayut.bo@ku.th>
2020-11-03 05:25:04 +0700

    update profile page action

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Address;
 use App\Models\User;
 use App\Rules\TelNumber;
 use Illuminate\Http\Request;
@@ -12,7 +11,10 @@ use Illuminate\Support\Facades\DB;
 class ProfileController extends Controller
 {
     public function index() {
-        $addresses = DB::table('addresses')->where("user_id", "=", Auth::user()->id)->get();
+        $addresses = DB::table('addresses')
+                    ->where("user_id", "=", Auth::user()->id)
+                    ->orderBy("default", "desc")
+                    ->get();
 
         return view('pages.profile', [
             'addrs' =>  $addresses
@@ -21,13 +23,13 @@ class ProfileController extends Controller
 
     public function editProfile(Request $request) {
         $request->validate([
-            'name' => ['required', 'max:30', 'string'],
-            'telephone' => ['required', 'max:10', new TelNumber]
+            'new_name' => ['required', 'max:30', 'string'],
+            'new_tel' => ['required', 'max:10', new TelNumber]
         ]);
 
         $user = User::findOrFail(Auth::user()->id);
         $user->name = $request->input('new_name');
-        $user->tel = $request->input('new_tel');
+        $user->telephone = $request->input('new_tel');
         $user->save();
 
         return redirect()->route('profile');
