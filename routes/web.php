be8411b6c9a1b3496c4aaef5508671f0611ce313
puen <surayut.bo@ku.th>
2020-11-07 21:13:24 +0700

    update route/web.php

--- routes/web.php
+++ routes/web.php
@@ -23,42 +23,48 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-//resource::
-Route::resource('product_list', ProductsController::class);
-Route::resource('address', AddressController::class);
-Route::resource('product_list', ProductsController::class);
-Route::resource('product_management', ProductsController::class);
-Route::resource('create_store',StoresController::class);
-Route::resource('stores',StoresController::class);
-Route::resource('product_list', ProductsController::class);
-Route::resource('create_store',StoresController::class);
+/*
+| GROUP SECTION BY CONTROLLER NAME
+| GROUP IN SECTION BY REQUEST METHOD
+*/
 
-//middleware
+
+// Authentication
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia\Inertia::render('Dashboard');
 })->name('dashboard');
 
-// get
+// Pages
 Route::get('/', [PagesController::class, 'index'])->name('pages.home');
+
+// Porfile
+Route::get('/profile', [ProfileController::class,'index'])->name('profile');
+Route::get('/profile/edit', [ProfileController::class, 'showEditProfile'])->name('edit-profile');
+Route::post('/profile/edit', [ProfileController::class,'editProfile'])->name('update-profile');
+
+// Address
+Route::resource('address', AddressController::class);
+Route::put('/address/change_default/{address}', [AddressController::class, 'changeDefaultAddress'])->name('changeDefaultAddress');
+
+// UserProduct
+Route::post('/user_product/{opt}', [UserProductController::class, 'showUserProduct']);
+
+// Cart
 Route::get('/cart', [CartController::class, 'index'])->name('cart');
+
+// Products
+Route::resource('product_list', ProductsController::class);
+
+// Stores
+Route::resource('stores',StoresController::class);
+
+// No Controller
 Route::get('/cart/checkout', function () {
     return view('pages.checkout');
 })->name('checkout');
-Route::get('/profile', [ProfileController::class,'index'])->name('profile');
-Route::get('/profile/edit', [ProfileController::class, 'showEditProfile'])->name('edit-profile');
 Route::get('/profile/open-shop', function () {
     return view('auth.seller_register');
 })->name('seller_register');
-Route::get('/product/{id}', [ProductsController::class,'productDetail'])->name('product_detail');
 Route::get('/order-details/1', function () {
     return view('pages.order_details');
 })->name('order_details');
-
-
-//post
-Route::post('/profile/edit', [ProfileController::class,'editProfile'])->name('update-profile');
-Route::post('/user_product/{opt}', [UserProductController::class, 'showUserProduct']);
-
-//put
-Route::put('/address/change_default/{address}', [AddressController::class, 'changeDefaultAddress'])->name('changeDefaultAddress');
-// Route::get('/edit_product/{product_list}', [ProductsController::class,'edit'])->name('edit_product');
