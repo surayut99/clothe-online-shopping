d5a7ea82e89a7389467e244e1c29b02a24a20ff6
Thanakrit Lochanti <thanakrit.lo@ku.th>
2020-11-11 20:18:34 +0700

    edit some bug in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,11 +16,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            // UserSeeder::class,
-            // AddressSeeder::class,
-            // StoreSeeder::class,
-            // ProductSeeder::class,
-            // CartSeeder::class
+            UserSeeder::class,
+            AddressSeeder::class,
+            StoreSeeder::class,
+            ProductSeeder::class,
+            CartSeeder::class,
             OrderSeeder::class,
             OrderDetailsSeeder::class
         ]);
--- database/seeders/OrderDetailsSeeder.php
+++ database/seeders/OrderDetailsSeeder.php
@@ -22,24 +22,24 @@ class OrderDetailsSeeder extends Seeder
         $detail->save();
 
         $detail = new OrderDetail();
-        $detail->order_id = 1;
-        $detail->product_id = 2;
-        $detail->qty = 20;
-        $detail->price = 300;
-        $detail->save();
-
-        $detail = new OrderDetail();
         $detail->order_id = 2;
         $detail->product_id = 1;
-        $detail->qty = 12;
-        $detail->price = 30;
-        $detail->save();
-
-        $detail = new OrderDetail();
-        $detail->order_id = 2;
-        $detail->product_id = 2;
         $detail->qty = 20;
         $detail->price = 300;
         $detail->save();
+
+        // $detail = new OrderDetail();
+        // $detail->order_id = 1;
+        // $detail->product_id = 3;
+        // $detail->qty = 12;
+        // $detail->price = 30;
+        // $detail->save();
+
+        // $detail = new OrderDetail();
+        // $detail->order_id = 1;
+        // $detail->product_id = 4;
+        // $detail->qty = 20;
+        // $detail->price = 300;
+        // $detail->save();
     }
 }
